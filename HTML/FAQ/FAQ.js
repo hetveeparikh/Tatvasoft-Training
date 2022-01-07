d33d24b200bd966c93fb79cfb51cc2715faee0a5
hetveeparikh <hetveeparikh9@gmail.com>
2022-01-07 13:13:57 +0530

    PSD to HTML

--- HTML/FAQ/FAQ.js
+++ HTML/FAQ/FAQ.js
@@ -12,3 +12,10 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
