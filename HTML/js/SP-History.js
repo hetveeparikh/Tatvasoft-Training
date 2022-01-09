259090617896ebc8584b28750e61f3b4c2fde8bb
hetveeparikh <hetveeparikh9@gmail.com>
2022-01-09 21:26:47 +0530

    PSD to HTML

---
+++ HTML/js/SP-History.js
@@ -0,0 +1,22 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+$(document).ready(function () {
+    $("#historysp").DataTable({
+      dom: '<"top">rt<"bottom"lip><"clear">',
+      responsive: true,
+      aaSorting: [],
+      columnDefs: [
+        {
+          orderable: false,
+          targets: 4,
+        },
+      ],
+    });
+  });
